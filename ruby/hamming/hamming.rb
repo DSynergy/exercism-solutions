7d810df5394074c9fb6bff012c04f01915b3eff7
Dustin Gault <Dustin.Gault@gmail.com>
2015-04-10 16:02:10 -0600

    refactor iteration million

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,8 +5,6 @@ class Hamming
     strand2 = strand2.chars
     zipped_strands = strand1.zip(strand2)
 
-    zipped_strands.count do |x,y|
-      x != y
-    end
+    zipped_strands.count {|x,y| x != y}
   end
 end
