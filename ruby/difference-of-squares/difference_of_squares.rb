c784e1c928d68721e90eb4524f3fd8af18281c41
Dustin Gault <Dustin.Gault@gmail.com>
2015-04-10 15:53:18 -0600

    difference

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,19 @@
+class Squares
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sums
+    (1..@number).reduce(:+)**2
+  end
+
+  def sum_of_squares
+    (1..@number).reduce(0) {|sum, number| sum + (number **2) }
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
+end
