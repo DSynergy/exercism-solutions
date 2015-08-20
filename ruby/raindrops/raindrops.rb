5f8f6ccb2e35120e731386176f481b3dd8c90251
Dustin Gault <Dustin.Gault@gmail.com>
2015-08-20 00:35:45 -0600

    much cleaner hash solution

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,28 +1,17 @@
 class Raindrops
   class << self
 
+    RAINDROPS = { 3 => "Pling",
+                  5 => "Plang",
+                  7 => "Plong" }
+
     def convert(number)
       number = number.to_i
-      case
-      when number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-        'PlingPlangPlong'
-      when number % 5 == 0 && number % 7 == 0
-        'PlangPlong'
-      when number % 3 == 0 && number % 7 == 0
-        'PlingPlong'
-      when number % 3 == 0 && number % 5 == 0
-        'PlingPlang'
-      when number % 3 == 0
-        'Pling'
-      when number % 5 == 0
-        'Plang'
-      when number % 7 == 0
-        'Plong'
-      else
-        number.to_s
-      end
+      sounds = RAINDROPS.select do |prime, sound|
+        number % prime == 0
+      end.values.join
+      sounds.empty? ? number.to_s : sounds
     end
-
 
   end
 end
