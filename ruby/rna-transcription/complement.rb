d960e5bfb18d506a6c764356a983e0b8d347904e
Dustin Gault <Dustin.Gault@gmail.com>
2015-08-20 01:06:31 -0600

    typo fix

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -14,14 +14,14 @@ class Complement
         }
 
   def self.of_dna(strands)
-    compliments = strands.chars.map do |n|
+    complements = strands.chars.map do |n|
       DNA.select do |dna|
         n == dna
       end.values
     end.join
 
-    if compliments.size == strands.size
-      compliments
+    if complements.size == strands.size
+      complements
     else
       raise ArgumentError
     end
@@ -30,14 +30,14 @@ class Complement
 #takes rna and makes dna complement
 
   def self.of_rna(strands)
-    compliments = strands.chars.map do |n|
+    complements = strands.chars.map do |n|
       RNA.select do |rna|
         n == rna
       end.values
     end.join
 
-    if compliments.size == strands.size
-      compliments
+    if complements.size == strands.size
+      complements
     else
       raise ArgumentError
     end
