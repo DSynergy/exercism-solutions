3b9125d02611554b699ade8ab7ea2da5985c8a12
Dustin Gault <Dustin.Gault@gmail.com>
2015-08-20 00:38:06 -0600

    refactoring

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -7,7 +7,7 @@ class Raindrops
 
     def convert(number)
       number = number.to_i
-      sounds = RAINDROPS.select do |prime, sound|
+      sounds = RAINDROPS.select do |prime|
         number % prime == 0
       end.values.join
       sounds.empty? ? number.to_s : sounds
